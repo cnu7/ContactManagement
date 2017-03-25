ba3410ac2ab4b1fb950cab148b27f3114d073bc2
cnu7 <srinivasan@cnu-macbook-pro.local>
2017-03-25 17:16:54 +0530

    Initial Commit

---
+++ ContactManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactManagement
+//
+//  Created by srinivasan on 25/03/17.
+//  Copyright © 2017 srinivasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
